9650ea3983989d9c2d3698144e84711329398166
qaywsx22 <moyseevich@web.de>
2025-08-16 22:22:38 +0200

    submit with javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-let sbm = document.querySelector('#contactForm');
-sbm?.addEventListener('submit', function (event) {
-  // event.preventDefault(); // Prevent default form submission
-  alert('Formular wurde erfolgreich gesendet!');
+async function sendData(formElement) {
+  const action = "https://api.staticforms.xyz/submit"
+  const formData = new FormData(formElement);
+  try {
+    const response = await fetch(action, {
+      method: "POST",
+      // Set the FormData instance as the request body
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+document.querySelectorAll("form").forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendData(event.target);
+  });
 });
